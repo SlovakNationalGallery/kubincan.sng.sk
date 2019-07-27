8a5255d9acf43ec9a12459fbfca30ac34c38e984
Martin Havala <martin.havala@gmail.com>
2019-07-27 23:08:43 +0200

    init version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kubincan.sng.sk
 Microsite dedicated to Arnold Peter Weisz-Kubínčan
+
+Project is based on [StoryMapJS](https://github.com/NUKnightLab/StoryMapJS)
---
+++ js/locale/sk.js
@@ -0,0 +1,1 @@
+VCO.Language={name:"Slovenčina",lang:"sk",messages:{loading:"Nahrávanie",wikipedia:"Z Wikipedie, slobodnej encyklopédie",start:"Prozkoumat"},buttons:{map_overview:"Prehľadová mapa",overview:"Prehľad",backtostart:"Späť na začiatok",collapse_toggle:"Skryť mapu",uncollapse_toggle:"Zobraziť mapu",swipe_to_navigate:"Navigujte potiahnutím prsta<br><span class='vco-button'>OK</span>"}};
